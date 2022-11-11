26798ccb956d5425862efc294af152b099107043
chelc <chelchik91@mail.ru>
2022-11-11 16:45:33 +0700

    bot inline keyboard some results

--- src/handlers/users/commands.py
+++ src/handlers/users/commands.py
@@ -1,6 +1,8 @@
 from aiogram import types
+from aiogram.types import InputFile, InputMediaPhoto
 
-from keyboards import command_default_keyboard, command_delete_keyboard
+from keyboards import command_default_keyboard, command_delete_keyboard, get_item_inline_keyboard, \
+    navigation_items_callback
 from loader import dp, db
 
 
@@ -34,6 +36,7 @@ async def answer_help_comand(message: types.Message):
 async def answer_add_comand(message: types.Message):
     await message.reply(text=f'Я не умею пока ничего добавлять')
 
+
 @dp.message_handler(content_types=['contact'])
 async def answer_contact_command(message: types.Message):
     if message.contact.user_id == message.from_user.id:
@@ -42,9 +45,34 @@ async def answer_contact_command(message: types.Message):
     else:
         await message.answer('Увы')
 
+
 @dp.message_handler(commands=['product'])
 @dp.message_handler(text=['Список товаров'])
 async def answer_menu_command(message: types.Message):
     first_product_info = db.select_info('Products', id=1)
     first_product_info = first_product_info[0]
-    _, name, count, photo_path = first_product_info
+    _, name, prise, count, photo_path = first_product_info
+    item_text = f'Название товара: {name}\n' \
+                f'Цена товара: {prise}\n' \
+                f'Количество товара: {count}'
+    photo = InputFile(path_or_bytesio=photo_path)
+    await message.answer_photo(photo=photo,
+                               caption=item_text,
+                               reply_markup=get_item_inline_keyboard())
+
+
+@dp.callback_query_handler(navigation_items_callback.filter(for_data='items'))
+async def see_new_item(call: types.CallbackQuery):
+    current_item_id = int(call.data.split(':')[-1])
+    first_item_info = db.select_info('Products', id=current_item_id)
+    first_item_info = first_item_info[0]
+    _, name, prise, count, photo_path = first_item_info
+    item_text = f'Название товара: {name}\n' \
+                f'Цена товара: {prise}\n' \
+                f'Количество товара: {count}'
+    photo = InputFile(path_or_bytesio=photo_path)
+    await bot.edit_message_media(media=InputMediaPhoto(media=photo,
+                                                       caption=item_text),
+                                 chat_id=call.message.chat.id,
+                                 message_id=call.message.message_id,
+                                 reply_markup=get_item_inline_keyboard(id=current_item_id))
--- src/keyboards/inlines/user_keyboards.py
+++ src/keyboards/inlines/user_keyboards.py
@@ -8,19 +8,29 @@ def get_item_inline_keyboard(id: int = 1) -> InlineKeyboardMarkup:
     item_inline_keyboard = InlineKeyboardMarkup()
     left_id = id - 1
     right_id = id + 1
-    btn_left = InlineKeyboardButton(text='<<<',
-                                    callback_data=navigation_items_callback.new(
-                                        for_data='items',
-                                        id=left_id)
-                                    )
-    btn_right = InlineKeyboardButton(text='>>>',
-                                     callback_data=navigation_items_callback.new(
-                                         for_data='items',
-                                         id=right_id)
-                                     )
     if id == 1:
+        btn_right = InlineKeyboardButton(text='>>>',
+                                         callback_data=navigation_items_callback.new(
+                                             for_data='items',
+                                             id=right_id)
+                                         )
         item_inline_keyboard.add(btn_right)
     elif id == db.get_items_count('Products'):
+        btn_left = InlineKeyboardButton(text='<<<',
+                                        callback_data=navigation_items_callback.new(
+                                            for_data='items',
+                                            id=left_id)
+                                        )
         item_inline_keyboard.add(btn_left)
     else:
+        btn_left = InlineKeyboardButton(text='<<<',
+                                        callback_data=navigation_items_callback.new(
+                                            for_data='items',
+                                            id=left_id)
+                                        )
+        btn_right = InlineKeyboardButton(text='>>>',
+                                         callback_data=navigation_items_callback.new(
+                                             for_data='items',
+                                             id=right_id)
+                                         )
         item_inline_keyboard.row(btn_left, btn_right)
